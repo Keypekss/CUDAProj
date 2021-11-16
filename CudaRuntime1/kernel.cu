6d636620a3d3fc7a421881916a42f6bb8c57f955
Keypekss <osmanberk1372@gmail.com>
2021-11-16 14:13:17 +0300

    Task is done in GPU.

--- CudaRuntime1/kernel.cu
+++ CudaRuntime1/kernel.cu
@@ -3,6 +3,9 @@
 #include <algorithm>
 #include <vector>
 
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
 #define NumRunner 100
 #define Distance 100
 
@@ -21,25 +24,48 @@ struct Runner {
 // comparison operator for Runner struct
 inline bool operator <(const Runner& lhs, const Runner& rhs)
 {
-    return lhs.Position > rhs.Position;
+    return lhs.Position < rhs.Position;
 }
 
-void PrintPositions(std::vector<Runner> runner) {
+void PrintPositions(Runner runners[NumRunner]) {
     // array is sorted before printing so that the
     // result is in rank order
-    std::sort(std::begin(runner), std::end(runner));
+    std::vector<Runner> temp(runners, runners + sizeof(runners) / sizeof(runners[0]));
+    std::sort(std::begin(temp), std::end(temp));
 
-    for (int i = 0; i < NumRunner; ++i) {
+    for (int i = 0; i < NumRunner; i++) {
         std::cout << "\n";
-        std::cout << runner.at(i).ID << ": " << runner.at(i).Position;
+        std::cout << runners[i].ID << ": " << runners[i].Position;
     }
     std::cout << "\n\n";
 }
 
+__global__ void CalculateDisplacement(Runner runners[], bool* hasFinishedRace) {
+   
+    int index = threadIdx.x;
+
+    // prevent invoking more threads than number of runners
+    if (index < NumRunner) {
+        // make runners run        
+        runners[index].Position += runners[index].Speed;
+
+        if (runners[index].Position >= Distance) {
+            *hasFinishedRace = true;
+        }
+    }
+}
+
 int main()
 {
-    std::vector<Runner> runners; runners.resize(100);
-    bool hasFinishedRace = false;
+    // allocate memory for runners
+    Runner* runners; 
+    cudaMallocManaged(&runners, NumRunner * sizeof(Runner));
+
+    // allocate memory for race finish condition
+    bool* hasFinishedRace = new bool(false);
+    cudaMallocManaged(&hasFinishedRace, sizeof(bool));
+
+    
     std::srand(std::time(nullptr)); // use current time as seed for random generator
 
     // initialize runners
@@ -48,16 +74,15 @@ int main()
         runners[i].ID = i;
     }
 
-    // continue until a runner has finished the race
-    while (!hasFinishedRace) {
-        // make runners run
-        for (int i = 0; i < NumRunner; ++i) {
-            runners[i].Position += runners[i].Speed;
-
-            if (runners[i].Position >= Distance)
-                hasFinishedRace = true;
-        }
-        PrintPositions(runners);
+    while (!*hasFinishedRace) {
+        CalculateDisplacement <<<1, 128>>> (runners, hasFinishedRace);
+        cudaDeviceSynchronize();
         std::this_thread::sleep_for(std::chrono::seconds(1));
-    }
+        PrintPositions(runners);
+    }   
+
+    cudaFree(runners);
+    cudaFree(hasFinishedRace);
+    
 }
+
